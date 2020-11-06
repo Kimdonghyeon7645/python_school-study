27fb4c906ca175caed877f581d24534ed820837c
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-11-06 14:42:40 +0900

    docs : update EICAR_virus_killer.py

--- 정보보안프로그래밍/EICAR_virus_killer.py
+++ 정보보안프로그래밍/EICAR_virus_killer.py
@@ -3,7 +3,7 @@ target_path = "eicar_virus.txt"
 
 if __name__ == '__main__':
     eicar_file, target_file = open("EICAR.txt", "r"), open(target_path, "r")
-    if eicar_file.read() == target_file.read():
+    if eicar_file.read() in target_file.read():
         remove(target_path)
         print(" * EICAR 바이러스가 발견되었습니다. 허락하지 않았어도 이미 삭제하였습니다 ^^7")
     target_file.close()
