069dd30e5fc83300183c903f6b8d87aee93b9eed
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-11-20 17:53:30 +0900

    code : update VirusKiller_with-Sys.py

--- 정보보안프로그래밍/VirusKiller_with-Sys.py
+++ 정보보안프로그래밍/VirusKiller_with-Sys.py
@@ -1,37 +1,38 @@
-def md5_hash(target: str) -> str:
-    import hashlib
+import hashlib
+import sys
+import os
 
-    md5 = hashlib.md5(target.encode("utf-8"))
-    print(md5)
+
+def md5_hash(target: str) -> str:
+    """  """
+    md5 = hashlib.md5()
+    md5.update(target.encode())
+    print(target, ":", md5.hexdigest())
     return md5.hexdigest()
 
 
 def get_filepath_from_sys() -> str:
-    import sys
-
     if len(sys.argv) < 2:
         print(f"<!> 실행시 입력한 인자값이 올바르지 않습니다! \n 인자값 : {sys.argv}")
     return sys.argv[1]
 
 
 def check_if_file_is_virus(filepath: str, virus_db: dict):
-    import os
-
     file = open(filepath, "r", encoding="utf-8")
-    print(file.read(), md5_hash(file.read()))
-    if md5_hash(file.read()) in virus_db.values():
-        file.close()
-        os.remove(filepath)
-        print(f"{filepath} 파일에 바이러스가 발견되어서 삭제하였습니다.")
-    else:
-        print("파일에 바이러스가 발견되지 않았습니다.")
+    target = md5_hash(file.read())
+    for virus in virus_db.values():
+        if virus in target:
+            file.close()
+            os.remove(filepath)
+            print(f"{filepath} 파일에 악성코드가 발견되어서 삭제하였습니다.")
+        else:
+            print("파일에 바이러스가 발견되지 않았습니다.")
 
 
 if __name__ == '__main__':
-    db = {
+    virus_dict = {
         1: md5_hash("감기"),
         2: md5_hash("후두염"),
         3: md5_hash("독감")
     }
-    print(db)
-    check_if_file_is_virus(get_filepath_from_sys(), db)
+    check_if_file_is_virus(get_filepath_from_sys(), virus_dict)
